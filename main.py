37bfdc91f80d6f8a3d3ec430327f029defbe2e9c
freeman <freemanirabaruta@gmail.com>
2020-04-19 21:00:51 +0200

    added news route and template

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, redirect # backend stuff
 import requests # get the request
+from operator import itemgetter
 from forms import enter   # form data
+import json
 app=Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 @app.route('/', methods=['GET', 'POST'])
@@ -34,6 +36,11 @@ def continents():
     elif 'message' in res:     
         return render_template('404.html',msg=res['message'])
     return render_template("continents.html",form=form,name=res["continent"],cases=res["cases"],todaycases=res["todayCases"],deaths=res["deaths"],todayDeaths=res["todayDeaths"],recovered=res["recovered"],active=res["active"],critical=res["critical"])
-
+@app.route('/news')
+def news():
+    res=requests.get("https://newsapi.org/v2/everything?q=Coronavirus;covid-19;quarantine&apiKey=9656a7dbfd1744f69f74e571c0223d64")
+    articles=json.loads(res.content)["articles"]
+    articles=sorted(articles,key=itemgetter('publishedAt'),reverse=True)
+    return render_template('news.html',articles=articles)
 if __name__ == '__main__':
     app.run(port=7080,debug=True)  # run on port 7080
